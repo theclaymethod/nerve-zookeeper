9464c30415945911118043daf6e09eee893b154e
theclaymethod <clayton.kim@gmail.com>
2014-09-19 11:45:52 -0700

    Edits to ruby script to generate json

--- run.sh
+++ run.sh
@@ -16,7 +16,30 @@ fi
 
 sed -i -e"s/%%INSTANCE_ID%%/${NERVE_INSTANCE}/" /nerve.conf.json
 #Consider starting with json file for upload
-env|egrep '\w+_PORT=tcp://'|sed -e's/\(.*\)_PORT=tcp:../\1:/'|ruby1.9.3 -e'require "json";NERVE_APP=ENV["NERVE_APP"];STDIN.each_line {|l| d=l.chomp.split(/:/); File.open("/nerve_services/#{d[0]}.json", "w") {|f| f.puts Hash["host",d[1],"port",d[2],"reporter_type","zookeeper","zk_hosts",ENV["ZK_PORT_2181_TCP_ADDR"],"ZK_port",ENV["ZK_PORT_2181_TCP_PORT"].to_i,"ZK_path","/nerve/services/#{NERVE_APP}/services","check_interval",2,"checks",[Hash["type","tcp","timeout",0.2,"rise",3,"fall",2]]].to_json}}'
+#env|egrep '\w+_PORT=tcp://'|sed -e's/\(.*\)_PORT=tcp:../\1:/'|ruby1.9.3 -e'require "json";NERVE_APP=ENV["NERVE_APP"];STDIN.each_line {|l| d=l.chomp.split(/:/); File.open("/nerve_services/#{d[0]}.json", "w") {|f| f.puts Hash["host",d[1],"port",d[2],"reporter_type","zookeeper","zk_hosts",[ENV["ZK_PORT_2181_TCP"]],"zk_path","/nerve/services/#{NERVE_APP}/services","check_interval",2,"checks",[Hash["type","tcp","timeout",0.2,"rise",3,"fall",2]]].to_json}}'
+env|egrep 'SMARTSTACK.+TCP='|sed -e's/\(.*\)_PORT_.*\_TCP=tcp:../\1:/'|ruby1.9.3 -e'
+require "json";
+
+NERVE_APP=ENV["NERVE_APP"];
+
+STDIN.each_line {|l| d=l.chomp.split(/:/);
+File.open("/nerve_services/#{d[0]}.json", "w") {
+	|f| f.puts Hash["host",d[1],
+	"port",d[2],
+	"reporter_type","zookeeper",
+	"zk_hosts", [ ENV["ZK_PORT_2181_TCP"] ],
+	"zk_path",	"/nerve/services/#{NERVE_APP}/services",
+	"check_interval",2,
+	"checks",[Hash["type","http",
+		"uri",d[1],
+		"port",d[2],
+		"timeout",0.2,
+		"rise",3,
+		"fall",2]]].to_json
+	#{}"checks",[Hash["type","tcp","timeout",0.2,"rise",3,"fall",2]]].to_json
+}
+}'
+
 rm -f /nerve_services/ZK.json
 
 SERVICECOUNT=$(ls /nerve_services | wc -l)
